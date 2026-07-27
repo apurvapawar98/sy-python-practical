d368030c728dcad3176200850c25eeb039232716
apurvapawar98 <pawarapurva616@gmail.com>
2026-07-27 13:21:44 +0530

    Add files via upload

---
+++ .trafffic signal.py
@@ -0,0 +1,9 @@
+print("___traffice signal ruel___")
+signal =input("enter the signal colour(red / yellow / green):")
+
+if signal =="red":
+    print("action:stop")
+elif signal =="yellow":
+    print("action:wait")
+elif signal =="green":
+    print("action:go")
