2134049a047e776c99afd7ee783181278fc0331b
apurvapawar98 <pawarapurva616@gmail.com>
2026-07-29 12:42:50 +0530

    Add files via upload

---
+++ student information.py
@@ -0,0 +1,15 @@
+studentname =input("enter your student name:")
+print(" student name is",studentname)
+
+age =int(input("enter your age:"))
+print("your age is:",age)
+
+collagename =(input("enter your collage name:"))
+print("your collage name is:",collagename)
+
+studentprn = (input("enter student prn: "))
+print("student prn is:",studentprn)
+
+studentemail = (input("enter student email id"))
+print("student email id is :",studentemail)
+
