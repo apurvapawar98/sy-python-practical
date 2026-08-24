e882e066ab22614592e9fd16b0d4e1e6a1423cce
apurvapawar98 <pawarapurva616@gmail.com>
2026-08-24 10:42:44 +0530

    Add files via upload

---
+++ ex1.2.py
@@ -0,0 +1,8 @@
+name = (input("enter employee name:"))
+role = (input("enter emplyee role:"))
+salary = int(input("enter emplyee salary:"))
+
+print("____emplyee salary card____")
+print("emplyee name",name)
+print("emplyee role",role)
+print("emplyee salary:",salary)
